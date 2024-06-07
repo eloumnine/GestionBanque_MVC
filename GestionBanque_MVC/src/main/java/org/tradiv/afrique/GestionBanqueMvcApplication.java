b58d60de48ac71797d96d98d14acb1dbd3cf3145
abdelhakim.eloumnine <eloumnine@gmail.com>
2024-06-07 09:55:16 +0100

    Initial Commit - Gestion Banque

---
+++ GestionBanque_MVC/src/main/java/org/tradiv/afrique/GestionBanqueMvcApplication.java
@@ -0,0 +1,13 @@
+package org.tradiv.afrique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionBanqueMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionBanqueMvcApplication.class, args);
+	}
+
+}
---
+++ GestionBanque_MVC/src/test/java/org/tradiv/afrique/GestionBanqueMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tradiv.afrique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionBanqueMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
